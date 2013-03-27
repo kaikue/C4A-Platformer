88141253808467e57767e948e52c410bfe0525ef
Kai Kuehner <kvk@pobox.com>
2013-03-27 16:01:51 -0400

    =Fixed LWJGL files

--- src/Engine.java
+++ src/Engine.java
@@ -1,10 +1,25 @@
-import org.lwjgl.test.WindowCreationTest;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
 public class Engine {
-
-    public static void main(String[] args) {
-        
-
+    public void start() {
+        try {
+            Display.setDisplayMode(new DisplayMode(800,600));
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        // init OpenGL here
+        while (!Display.isCloseRequested()) {
+            // render OpenGL here
+            Display.update();
+        }
+        Display.destroy();
     }
-
+    public static void main(String[] argv) {
+        Engine displayExample = new Engine();
+        displayExample.start();
+    }
 }
